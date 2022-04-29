1f98898fe26440f0642e60366d50d6e63a979d77
Kerry Long <victory173451@gmail.com>
2022-04-29 11:41:00 -0400

    searchBook

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar';
 
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
