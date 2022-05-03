5fd5bf31e38b337990d9e3e0d249c70dca0c775a
Kerry Long <victory173451@gmail.com>
2022-05-02 23:56:46 -0400

    check for production server code

--- server/server.js
+++ server/server.js
@@ -28,6 +28,16 @@ await server.start();
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
